10bf4c6778ed323df7f8c55828829107d3402e84
byeongjulee222 <dave.juya777@gmail.com>
2019-07-09 12:11:31 +0900

    second commit

--- 00_startcamp/02_day/01_naver_search.py
+++ 00_startcamp/02_day/01_naver_search.py
@@ -10,4 +10,4 @@ searches = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_h
 for i in searches:   #for문으로 돌리기
     print(i.text)    #.text로 필요한 자료만 추출
 
-#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li:nth-child(4) > a > span.ah_k
+# 두번째 커밋을 위한 주석!!!
