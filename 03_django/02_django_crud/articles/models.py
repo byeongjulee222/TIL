9a8195809cc27ac0334351c112e44ccf08dbd3a6
byeongjulee222 <dave.juya777@gmail.com>
2019-09-18 18:00:41 +0900

    190918

--- 03_django/02_django_crud/articles/models.py
+++ 03_django/02_django_crud/articles/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,13 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        # return f'/articles/{self.pk}/'
+        return reverse('articles:detail', args=[self.pk]) # /articles/10/
+        '''
+        딕셔너리로 작성할 때
+        return reverse('articles:detail', kwargs={'pk':self.pk}) # /articles/10/
+        '''
+        # 주의사항
+        # reverse 함수에 args랑 kwargs를 동시에 인자로 보낼 수 없다.
--- 03_django/02_django_crud/articles/views.py
+++ 03_django/02_django_crud/articles/views.py
@@ -1,4 +1,5 @@
 from IPython import embed
+# embed : 서버 잠깐 멈추게하는 것
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from .models import Article
@@ -13,60 +14,44 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 
-def new(request):
-    return render(request, 'articles/new.html')
-
-
 # 직접적으로 모델에 저장하는 역할
 def create(request):
-    # 서버를 일시정지 시켜주는 역할
-    # embed()
-    # 검증 단계 추가
-    try:
+    # CREATE
+    if request.method == 'POST':
         title = request.POST.get('title')
-        content = request.POST.get('content')
-        
-        # 데이터를 저장하는 첫 번째 방법
-        # article = Article()
-        # article.title = title
-        # article.content = content
-        # article.save()
-
-        # 두 번째 방법
+        content = request.POST.get('content') 
         article = Article(title=title, content=content)
-        # 검증 연결
-        article.full_clean()
-    except ValidationError:
-        raise ValidationError('Error')
-    # 아무 오류가 발생하지 않았을 때
-    else:
         article.save()
-        # 세 번째 방법
-        # Article.objects.create(title=title, content=content)
-        return redirect(f'/articles/{article.pk}') # 메인페이지 (index값으로 전환)
+        return redirect(article)
+    # NEW
+    else:
+        return render(request, 'articles/create.html')
 
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
+    # article.get_absolute_url()
     context = {'article': article,}
     return render(request, 'articles/detail.html', context)
 
 
 def delete(request, pk):
     article = Article.objects.get(pk=pk)
-    article.delete()
-    return redirect('/articles/')
-
-
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {'article': article,}
-    return render(request, 'articles/edit.html', context)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
 
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    return redirect(f'/articles/{article.pk}')
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        context = {'article': article,}
+        return render(request, 'articles/update.html', context)
+
