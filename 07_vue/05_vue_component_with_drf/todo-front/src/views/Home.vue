cd614ca27a32a7616b4c6092ba66c90e4f6c5ed4
byeongjulee222 <dave.juya777@gmail.com>
2019-11-29 15:47:25 +0900

    191129

--- 07_vue/05_vue_component_with_drf/todo-front/src/views/Home.vue
+++ 07_vue/05_vue_component_with_drf/todo-front/src/views/Home.vue
@@ -9,9 +9,10 @@
 <script>
 import router from '../router'
 import TodoList from '@/components/TodoList'
-import axios from 'axios'
-import jwtDecode from 'jwt-decode'
 import TodoInput from '@/components/TodoInput'
+import axios from 'axios'
+// import jwtDecode from 'jwt-decode'
+import { mapGetters } from 'vuex'
 // @ is an alias to /src
 
 export default {
@@ -24,24 +25,34 @@ export default {
       todos: [],
     }
   },
+  computed: {
+    // '...' spread 문법 -> 각각의 getters 가져옴
+    // mapGetters 함수의 인자로 들어가는 배열에는 getters에서 정의한 함수들 중에서
+    // 가져오고 싶은 getter 들을 작성한다.
+    ...mapGetters([
+      'isLoggedIn',
+      'requestHeader',
+      'userId'
+    ])
+  },
   methods: {
     checkLoggedIn() {
-      this.$session.start()
-      if (!this.$session.has('jwt')) {
+      // this.$session.start()
+      if (!this.isLoggedIn) {
         router.push('/login')
       }
     },
     getTodos() {
-      this.$session.start()
-      const token = this.$session.get('jwt')
-      const requestHeader = {
-        headers: {
-          Authorization: 'JWT ' + token
-        }
-      }
-      const user_id = jwtDecode(token).user_id
-      console.log(jwtDecode(token))
-      axios.get(`http://127.0.0.1:8000/api/v1/users/${user_id}/`, requestHeader)
+      // this.$session.start()
+      // const token = this.$session.get('jwt')
+      // const requestHeader = {
+      //   headers: {
+      //     Authorization: 'JWT ' + token
+      //   }
+      // }
+      // const user_id = jwtDecode(token).user_id
+      // console.log(jwtDecode(token))
+      axios.get(`http://127.0.0.1:8000/api/v1/users/${this.userId}/`, this.requestHeader)
         .then(res => {
           console.log(res)
           this.todos = res.data.todo_set
@@ -51,19 +62,19 @@ export default {
         })
     },
     createTodo(title) {
-      this.$session.start()
-      const token = this.$session.get('jwt')
-      const requestHeader = {
-        headers: {
-          Authorization: 'JWT ' + token
-        }
-      }
-      const user_id = jwtDecode(token).user_id
+      // this.$session.start()
+      // const token = this.$session.get('jwt')
+      // const requestHeader = {
+      //   headers: {
+      //     Authorization: 'JWT ' + token
+      //   }
+      // }
+      // const user_id = jwtDecode(token).user_id
       const requestForm = new FormData()
-      requestForm.append('user', user_id)
+      requestForm.append('user', this.userId)
       requestForm.append('title', title)
 
-      axios.post('http://127.0.0.1:8000/api/v1/todos/', requestForm, requestHeader)
+      axios.post('http://127.0.0.1:8000/api/v1/todos/', requestForm, this.requestHeader)
         .then(res => {
           this.todos.push(res.data)
           console.log(res)
